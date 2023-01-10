ac1ae97c2bcb618df9799c5c07c6f97c70b8c18a
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-01-10 15:34:47 +0530

    Dev Dockerfile added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Segment, Image } from "semantic-ui-react";
+// import PluggLogo from "../img/pluggsvg.svg";
 import PluggLogo from "../img/logo.png";
 
 const Footer = () => {
